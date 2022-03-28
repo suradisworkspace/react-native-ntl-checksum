ce1c7e97780c0e0ea56368037ba8f39f2ac3f3ad
Suradis Sutampang <suradis.s@tidlor.com>
2022-03-28 15:36:45 +0700

    fix checksum

--- android/src/main/java/com/bundlechecksum/RNBundleChecksumModule.java
+++ android/src/main/java/com/bundlechecksum/RNBundleChecksumModule.java
@@ -1,8 +1,6 @@
 
 package com.bundlechecksum;
 
-import android.app.Activity;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.content.res.AssetManager;
@@ -11,17 +9,14 @@ import android.util.Log;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class RNBundleChecksumModule extends ReactContextBaseJavaModule {
 
@@ -40,8 +35,7 @@ public class RNBundleChecksumModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void getChecksum(Promise promise) {
-        BufferedReader br = null;
-        String bundle = null;
+        byte[] hash = null;
         StringBuilder sb = new StringBuilder();
         try {
             AssetManager assetManager = getReactApplicationContext().getAssets();
@@ -50,17 +44,16 @@ public class RNBundleChecksumModule extends ReactContextBaseJavaModule {
                 promise.resolve("");
                 return;
             }
-
             int size = stream.available();
             byte[] buffer = new byte[size];
             stream.read(buffer);
             stream.close();
-            bundle = new String(buffer);
-
-            if (bundle == null || bundle.equals("")) {
+            hash = MessageDigest.getInstance("SHA-256").digest(buffer);
+            if (hash == null) {
                 promise.resolve("");
                 return;
             }
+
         } catch (Exception ex) {
             ex.printStackTrace();
             promise.resolve("");
@@ -68,25 +61,12 @@ public class RNBundleChecksumModule extends ReactContextBaseJavaModule {
         }
 
         MessageDigest md = null;
-        sb = new StringBuilder();
         try {
-            md = MessageDigest.getInstance("SHA-256");
-            md.update(bundle.getBytes("UTF-8"));
-            byte[] digest = md.digest();
-
-            for (int i = 0; i < digest.length; i++) {
-                sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+            for (int i = 0; i < hash.length; i++) {
+                sb.append(Integer.toString((hash[i] & 0xff) + 0x100, 16).substring(1));
             }
 
             promise.resolve(sb.toString());
-            return;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            promise.resolve("");
-            return;
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            promise.resolve("");
             return;
         } catch (Exception e) {
             e.printStackTrace();
@@ -97,8 +77,7 @@ public class RNBundleChecksumModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void getChecksumCert(String certName, Promise promise) {
-        BufferedReader br = null;
-        String bundle = null;
+        byte[] hash = null;
         StringBuilder sb = new StringBuilder();
         try {
             AssetManager assetManager = getReactApplicationContext().getAssets();
@@ -112,9 +91,8 @@ public class RNBundleChecksumModule extends ReactContextBaseJavaModule {
             byte[] buffer = new byte[size];
             stream.read(buffer);
             stream.close();
-            bundle = new String(buffer);
-
-            if (bundle == null || bundle.equals("")) {
+            hash = MessageDigest.getInstance("SHA-256").digest(buffer);
+            if (hash == null) {
                 promise.resolve("");
                 return;
             }
@@ -124,26 +102,12 @@ public class RNBundleChecksumModule extends ReactContextBaseJavaModule {
             return;
         }
 
-        MessageDigest md = null;
-        sb = new StringBuilder();
         try {
-            md = MessageDigest.getInstance("SHA-256");
-            md.update(bundle.getBytes("UTF-8"));
-            byte[] digest = md.digest();
-
-            for (int i = 0; i < digest.length; i++) {
-                sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
+            for (int i = 0; i < hash.length; i++) {
+                sb.append(Integer.toString((hash[i] & 0xff) + 0x100, 16).substring(1));
             }
 
             promise.resolve(sb.toString());
-            return;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            promise.resolve("");
-            return;
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            promise.resolve("");
             return;
         } catch (Exception e) {
             e.printStackTrace();
@@ -154,15 +118,17 @@ public class RNBundleChecksumModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void getSumMETA(Promise promise) {
-        try {
-            Signature sigs = reactContext.getPackageManager().getPackageInfo(reactContext.getPackageName(), PackageManager.GET_SIGNATURES).signatures[0];
-
-            promise.resolve(sigs.hashCode());
-        }catch (Exception e){
-            promise.resolve("");
-        }
-
+        promise.resolve("");
         return;
+//        try {
+//            Signature sigs = reactContext.getPackageManager().getPackageInfo(reactContext.getPackageName(), PackageManager.GET_SIGNATURES).signatures[0];
+//
+//            promise.resolve(sigs.hashCode());
+//        } catch (Exception e) {
+//            promise.resolve("");
+//        }
+//
+//        return;
     }
 
 
